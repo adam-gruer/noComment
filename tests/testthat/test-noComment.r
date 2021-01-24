cf65bde4451355aa1d0f814a6943a727fcb84c4b
Adam Gruer <Adam.Gruer@epa.vic.gov.au>
2021-01-24 22:50:27 +1100

    Update test

--- tests/testthat/test-noComment.r
+++ tests/testthat/test-noComment.r
@@ -4,6 +4,8 @@ test_that("Remove commented  characters returns evaluatable code", {
   noComments <- removeCommentCharacters(commented_code)
 #to do  sourcetools::tokenize_string(noComments)
 
-  expect_equal(removeCommentCharacters(commented_code), c("1 + 1",  " 3 + 3"))
+  expect_equal(eval(str2lang(noComments[1])), 2)
+  expect_equal(eval(str2lang(noComments[2])), 6)
+
 
 })
