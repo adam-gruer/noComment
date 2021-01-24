cd323fcee2ed54b3e50a554b337886bca5acae23
Adam Gruer <Adam.Gruer@epa.vic.gov.au>
2021-01-24 14:43:12 +1100

    Document package

--- tests/testthat/test-noComment.r
+++ tests/testthat/test-noComment.r
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Remove commented  characters returns evaluatable code", {
+
+  commented_code = c("# 1 + 1",  "#' 3 + 3")
+  noComments <- removeCommentCharacters(commented_code)
+#to do  sourcetools::tokenize_string(noComments)
+
+  expect_equal(removeCommentCharacters(commented_code), c("1 + 1",  " 3 + 3"))
+
 })
